b802b391d971aef33bc83eb8df8defc9675a689e
Bajka Bogunovic <bajkabogunovic@gmail.com>
2019-02-08 15:43:54 +0100

    Completed the module

---
+++ src/components/BookItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <li>{{book.title}}:{{book.author}}</li>
+</template>
+
+<script>
+
+</script>
+export default {
+  name: 'BookItem',
+  props: ['book'],
+}
+<style>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
